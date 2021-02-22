6c4aa7771d3c666b9bf1961c2003659e25e6dcb9
42azimut <azimutuniverse@gmail.com>
2021-02-22 23:36:56 +0900

    3-02게시판 페이징 기능 추가

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -3,11 +3,17 @@ from django.http import HttpResponse
 from .models import Question
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
     question_list = Question.objects.order_by('-create_date')
     context = {'question_list' : question_list}
+    page = request.GET.get('page', '1') # page
+    question_list = Question.objects.order_by('-create_date')
+    paginator = Paginator(question_list, 10)  #페이지당 10개씩 보이기
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 
